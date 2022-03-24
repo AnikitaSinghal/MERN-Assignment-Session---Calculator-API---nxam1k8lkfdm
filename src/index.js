dc1e6586f2be039acc8ed73f7004b535d36c1197
Anikita Singhal <92014545+AnikitaSinghal@users.noreply.github.com>
2022-03-24 15:04:25 +0530

    completed

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ app.post("/add",(req,res)=>{
     if(num1<lowerlimit||num2<lowerlimit)
     res.status(400).send({
         status:"Error",
-        message:"Underflolw"
+        message:"Underflow"
         });
     if(typeof num1!=="number" || typeof num2!=="number")
     res.status(400).send({
@@ -34,6 +34,52 @@ app.post("/add",(req,res)=>{
         status:"Success",
         message:"the sum of given two numbers",
         sum:num1+num2,
+    });
+});
+app.post("/subtract",(req,res)=>{
+    const {num1,num2}=req.body
+    if(num1>upperlimit||num2>upperlimit)
+    res.status(400).send({
+        status:"Error",
+        message:"Overflow"
+        });
+    if(num1<lowerlimit||num2<lowerlimit)
+    res.status(400).send({
+        status:"Error",
+        message:"Underflow"
+        });
+    if(typeof num1!=="number" || typeof num2!=="number")
+    res.status(400).send({
+        status:"Error",
+        message:"Invalid Data types"
+        });
+    res.status(200).send({
+        status:"Success",
+        message:"the difference of given two numbers",
+        difference:num1-num2,
+    });
+});
+app.post("/multiply",(req,res)=>{
+    const {num1,num2}=req.body
+    if(num1>upperlimit||num2>upperlimit)
+    res.status(400).send({
+        status:"Error",
+        message:"Overflow"
+        });
+    if(num1<lowerlimit||num2<lowerlimit)
+    res.status(400).send({
+        status:"Error",
+        message:"Underflolw"
+        });
+    if(typeof num1!=="number" || typeof num2!=="number")
+    res.status(400).send({
+        status:"Error",
+        message:"Invalid Data types"
+        });
+    res.status(200).send({
+        status:"Success",
+        message:"the sum of given two numbers",
+        result: num1*num2,
     });
 });
 
