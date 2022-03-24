b66e893a8f059f59c2549bdd211cb63c10799505
Anikita Singhal <92014545+AnikitaSinghal@users.noreply.github.com>
2022-03-24 12:46:07 +0530

    completed

--- src/index.js
+++ src/index.js
@@ -11,13 +11,22 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
+
+app.post("/add",(req,res)=>{
+    const numbers=req.body
+    const sum=numbers.num1+numbers.num2;
+    res.send(200).send({
+        status:"Success",
+        message:"the sum of given two numbers",
+        sum: sum
+    })
+})
+
 // your code goes here
 app.get("/",(req,res)=>{
     res.status(200).send("Hello World")
 })
-app.post("/add",(req,res)=>{
 
-})
 app.post("/subtract",(req,res)=>{
 
 })
@@ -25,7 +34,7 @@ app.post("/multiply",(req,res)=>{
 
 })
 app.post("/divide",(req,res)=>{
-    
+
 })
 
 
